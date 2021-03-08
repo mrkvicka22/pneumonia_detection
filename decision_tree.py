d1842e585f753d7ec2babdbd67ec8464357816f4
Matej Novosad <40635376+mrkvicka22@users.noreply.github.com>
2021-03-08 19:47:31 +0100

    Added evaluation with k cross validation
    Tuning max_depth and min_samples_leaf

--- decision_tree.py
+++ decision_tree.py
@@ -1,37 +1,14 @@
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from create_datasets import DataSet
 import numpy as np
 
 
-def binarySearch(arr, l, r, x):
-    while l <= r:
-
-        mid = l + (r - l) // 2;
-
-        # Check if x is present at mid
-        if arr[mid] == x:
-            return arr[mid]
-
-            # If x is greater, ignore left half
-        elif arr[mid] < x:
-            l = mid + 1
-
-        # If x is smaller, ignore right half
-        else:
-            r = mid - 1
-
-    # If we reach here, then the element
-    # was not present
-    return arr[l]
+tree_config = {"max_depth":list(range(3,35,5)),
+               "min_samples_leaf":list(range(1,50,5)),
+               }
 
 
-# Driver Code
-arr = [2, 3, 4, 10, 40]
-x = 20
-result = binarySearch(arr, 0, len(arr) - 1, x)
-print(result)
-exit()
 whole_dataset = DataSet()
 whole_dataset.load(r"C:\Users\Matej\PycharmProjects\pneumonia_detection\pneumonia_data_pickled")
 train_data_images, train_data_labels = whole_dataset.shuffled_batches("train", batchsize=0)
@@ -57,7 +34,16 @@ all_y = np.concatenate((train_y, test_y, val_y))
 #
 #
 
-clf = DecisionTreeClassifier(random_state=0, min_samples_leaf=5, class_weight="balanced")
+tree = DecisionTreeClassifier(random_state=0, min_samples_leaf=5, class_weight="balanced")
 # clf.fit(val_X, val_y)
-scores = cross_val_score(clf, all_X, all_y, n_jobs=6, cv=10, scoring='precision')
-print(scores)
+clf = GridSearchCV(tree, tree_config, verbose=3,n_jobs=10)
+clf.fit(all_X, all_y)
+
+print(clf.cv_results_,clf.best_params_)
+'''
+max_depth=13, min_samples_leaf=31, score=0.835, total=12.5min
+max_depth=13, min_samples_leaf=41, score=0.835, total=13.0min
+'''
+
+
+
