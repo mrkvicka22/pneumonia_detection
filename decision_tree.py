e86194f73422f6d6a4e62ce9c67c1cee6b38bea0
Matej Novosad <40635376+mrkvicka22@users.noreply.github.com>
2021-03-15 23:21:18 +0100

    Made hyperparamter testing easier using config dicitonary
    Added config dicitionary that makes testing hyperparameters easier among all algorithms altough, this commit is only for decision trees.

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,4 @@
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score, GridSearchCV
 from create_datasets import DataSet
 import numpy as np
 import time
@@ -17,6 +16,7 @@ val_data_images, val_data_labels = whole_dataset.shuffled_batches("val", batchsi
 
 train_X = [img.flatten() for img in train_data_images]
 train_y = train_data_labels
+print(len(train_y))
 
 test_X = [img.flatten() for img in test_data_images]
 test_y = test_data_labels
@@ -27,24 +27,27 @@ val_y = val_data_labels
 all_X = np.concatenate((train_X, test_X, val_X))
 all_y = np.concatenate((train_y, test_y, val_y))
 
-# X, y = make_classification(n_samples=1000, n_features=4,
-#                            n_informative=2, n_redundant=0,
-#                            random_state=0, shuffle=False)
-#
-#
-#
 
-# tree = DecisionTreeClassifier(random_state=0, min_samples_leaf=5, class_weight="balanced")
-# # clf.fit(val_X, val_y)
-# clf = GridSearchCV(tree, tree_config, verbose=3,n_jobs=-1)
-# clf.fit(all_X, all_y)
-#
-# print(clf.cv_results_,clf.best_params_)
 '''
 max_depht=13, min_samples_leaf=26
 max_depth=13, min_samples_leaf=31, score=0.835, total=12.5min
 max_depth=13, min_samples_leaf=41, score=0.835, total=13.0min
 '''
+
+
+def comb(poss_list):
+    if type(poss_list) == dict:
+        poss_list = list(poss_list.values())
+    start = [[i] for i in poss_list[0]]
+    i = 1
+    while i < len(poss_list):
+        new = []
+        for thing in start:
+            for item in poss_list[i]:
+                new.append(thing + [item])
+        start = new
+        i += 1
+    return start
 
 
 def validate_alg(classifier):
@@ -59,25 +62,39 @@ def validate_alg(classifier):
     return correct / len(val_y)
 
 
+def validate_on_training(classifier):
+    correct = 0
+    for sample, label in zip(train_X[:1000], train_y[:1000]):
+        prediction = classifier.predict([sample])
+        if prediction == label:
+            correct += 1
+        else:
+            pass
+            # possible to add samples that the algorithm did not guess
+    return correct / len(train_y[:1000])
+
 best_guys = []
 max_score = 0
 best_guy = None
-for crit in ["gini","entropy"]:
-    for depth in range(6,35,4):
-        for leaves in range(20,50,4):
-            tree = DecisionTreeClassifier(random_state=0,max_depth=depth, min_samples_leaf=leaves, class_weight="balanced",criterion=crit)
-            start_time = time.time()
-            tree.fit(train_X,train_y)
-            train_time = time.time() - start_time
-            score = validate_alg(tree)
-            print(f"accuracy= {score}", f"depth={depth}", f"leaves={leaves}", f"training_time= {train_time}s", end=" ")
-            if score > max_score:
-                max_score = score
-                best_guys.append([tree,score])
-                best_guy = tree
-                print("new max")
-            elif score > 0.75:
-                best_guys.append([tree, score])
-                print("decent")
-            else:
-                print("and it was subpar")
+config = {"max_depth": list(range(10, 20)), "min_samples_leaf": list(range(2, 20))}
+for setup in comb(config):
+    dicc = dict(zip(config.keys(),setup))
+    tree = DecisionTreeClassifier(**dicc)
+    start_time = time.time()
+    tree.fit(train_X,train_y)
+
+    train_time = time.time() - start_time
+    score = validate_alg(tree)
+    train_data_score = validate_on_training(tree)
+    print(f"setup= {dicc}",f"accuracy= {round(score,3)}", f"train_accuracy= {round(train_data_score, 3)}", f"training_time= {round(train_time,3)}s", end=" ")
+    if score > max_score:
+        max_score = score
+        best_guys.append([tree,score])
+        best_guy = tree
+        print("new max")
+    elif score > 0.75:
+        best_guys.append([tree, score])
+        print("decent")
+    else:
+        print("subpar")
+print(sorted(best_guys, key=lambda x: x[1])[-10:])
